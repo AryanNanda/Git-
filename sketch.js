1b3871a8758ce0358f1bc6297aacae2a0499234f
Aryan Nanda <nanda.aryan2006@gmail.com>
2020-06-01 15:15:42 +0530

    platform added by Aryan

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
@@ -15,10 +16,9 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
+    platform = new Ground(200,300,400,200)
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
-
+    
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -49,7 +49,7 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
-
+   
     box3.display();
     box4.display();
     pig3.display();
